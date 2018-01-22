225dad6301a3f4a6ec60cc27afbf01fe6c70f3b1
hugo <chenpengren@0easy.com>
2018-01-22 09:38:42 +0800

    rc

--- endpoint.py
+++ endpoint.py
@@ -15,18 +15,26 @@ schedule = Blueprint('schedule')
 @schedule.route('/api/v1/schedule/file_sync/', methods=['GET', 'POST'])
 @auth('token')
 async def file_sync(req):
-        target = req.json.get('target')
-        file_name = req.json.get('file_name')
-        jid = str(uuid.uuid3(uuid.NAMESPACE_DNS, str(int(time.time() * 100000000000000000000000000000))))
-        await sync(jid, target, file_name)
+    """
+    指定同步目录
+    1.单节点,单文件同步 node_id, file_name
+    2.单节点, 所有文件同步 node_id, *
+    3.所有节点, 所有文件同步 *, *
+    :param req:
+    :return:
+    """
+    target = req.json.get('target')
+    file_name = req.json.get('file_name')
+    jid = str(uuid.uuid3(uuid.NAMESPACE_DNS, str(int(time.time() * 100000000000000000000000000000))))
+    await sync(jid, target, file_name)
 
-        @retry_wait(retry_count=90, interval_wait=2)
-        def wait_result():
-            r = Redis(1).get(jid)
-            if not r:
-                raise Exception("还获取不到值，重试3分钟")
-            return r
-        return wait_result()
+    @retry_wait(retry_count=90, interval_wait=2)
+    def wait_result():
+        r = Redis(1).get(jid)
+        if not r:
+            raise Exception("还获取不到任务，重试等待3分钟")
+        return r
+    return wait_result()
 
 
 @schedule.exception(NotFound)
