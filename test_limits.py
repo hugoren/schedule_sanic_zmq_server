3f49dd7940f0babdfe3c96d4d23151564190f46a
hugo <chenpengren@0easy.com>
2018-02-22 12:04:05 +0800

    加入limit测试

--- test_limits.py
+++ test_limits.py
@@ -1,7 +1,16 @@
 from limits.storage import RedisStorage
 from limits.strategies import MovingWindowRateLimiter
+from limits import RateLimitItemPerSecond
 
-redis_storage = RedisStorage("redis://127.0.0.1/3")
-moving_window = MovingWindowRateLimiter(redis_storage)
-a = moving_window
-print(moving_window)
+
+def test():
+    storage = RedisStorage("redis://127.0.0.1/3")
+    strategy = MovingWindowRateLimiter(storage)
+    one_per_second = RateLimitItemPerSecond(10, 1)
+    r = strategy.hit(one_per_second, "k", "v")
+    print(r)
+
+for i in range(100):
+    test()
+
+
