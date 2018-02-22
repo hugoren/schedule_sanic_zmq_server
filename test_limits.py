ea1bc73b3541a721a511bb1bb2e330ffca60f095
hugo <chenpengren@0easy.com>
2018-02-22 12:17:20 +0800

    limits 三种根据k,v 三种策略

--- test_limits.py
+++ test_limits.py
@@ -1,14 +1,17 @@
 from limits.storage import RedisStorage
-from limits.strategies import MovingWindowRateLimiter
+from limits.strategies import MovingWindowRateLimiter, FixedWindowElasticExpiryRateLimiter
 from limits import RateLimitItemPerSecond
 
 
 def test():
     storage = RedisStorage("redis://127.0.0.1/3")
-    strategy = MovingWindowRateLimiter(storage)
-    one_per_second = RateLimitItemPerSecond(10, 1)
+    strategy = FixedWindowElasticExpiryRateLimiter(storage)
+    one_per_second = RateLimitItemPerSecond(20, 1)
     r = strategy.hit(one_per_second, "k", "v")
-    print(r)
+    if r == True :
+        print(True)
+    else:
+        print("请求满了")
 
 for i in range(100):
     test()
