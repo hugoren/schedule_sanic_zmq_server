a8e32f57d35acebf69257c47c22abc16c15c372d
hugo <chenpengren@0easy.com>
2018-02-23 17:46:50 +0800

    limits测试

--- test_limits.py
+++ test_limits.py
@@ -1,13 +1,14 @@
 from limits.storage import RedisStorage
 from limits.strategies import MovingWindowRateLimiter, FixedWindowElasticExpiryRateLimiter
-from limits import RateLimitItemPerSecond
+from limits import RateLimitItemPerSecond, RateLimitItemPerMinute
 
 
 def test():
     storage = RedisStorage("redis://127.0.0.1/3")
     strategy = FixedWindowElasticExpiryRateLimiter(storage)
-    one_per_second = RateLimitItemPerSecond(20, 1)
-    r = strategy.hit(one_per_second, "k", "v")
+    # one_per_second = RateLimitItemPerSecond(20, 1)
+    one_per_min = RateLimitItemPerMinute(100, 1)
+    r = strategy.hit(one_per_min, "k", "v")
     if r == True :
         print(True)
     else:
